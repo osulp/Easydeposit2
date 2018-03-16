168d02ef90b6c80a7b7959ffd2386ad48c65004d
Hui Zhang <hui.zhang@oregonstate.edu>
2018-03-16 16:46:11 -0700

    Swtich to Rubymine and troubleshoot route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
-Rails::Application.routes.draw do
-  if Rails.env.development?
-    mount RailsDb::Engine => '/rails/db', :as => 'rails_db'
-  end
+Rails.application.routes.draw do
+  get '/publications' => 'publications#index'
+  get '/publications/sourcelookup' => 'publications#sourcelookup'
+
+  ##
+  # Endpoint for Author harvester
+  post '/authors/:cap_profile_id/harvest', to: 'authors#harvest', defaults: { format: :json }
+
 end
