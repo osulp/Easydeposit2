211a00bbdba2cb231e356003f87773b47e10540f
Josh Gum <revgum@gmail.com>
2018-07-13 14:43:32 -0700

    defaults to requiring rails_helper for all specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,9 @@
+require 'spec_helper'
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'spec_helper'
 require 'rspec/rails'
 require 'webmock/rspec'
 require 'database_cleaner'
