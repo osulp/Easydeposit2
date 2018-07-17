72258e5f998c7d058d6f6aaaa55d98dc61659de1
Josh Gum <revgum@gmail.com>
2018-07-17 07:29:46 -0700

    adds OSU API for querying directory information

---
+++ lib/osu_api/client.rb
@@ -0,0 +1,87 @@
+# frozen_string_literal: true
+
+require 'faraday'
+require 'json'
+
+module OsuApi
+  OSU_API_URL = 'OSU_API_URL'
+  OSU_API_OAUTH2_TOKEN = 'OSU_API_OAUTH2_TOKEN'
+  OSU_API_DIRECTORY_SEARCH = 'OSU_API_DIRECTORY_SEARCH'
+  OSU_API_CONSUMER_KEY = 'OSU_API_CONSUMER_KEY'
+  OSU_API_CONSUMER_SECRET = 'OSU_API_CONSUMER_SECRET'
+
+  ##
+  # OSU API Client for querying Person Directory
+  class Client
+    def initialize
+      @key = ENV.fetch(OSU_API_CONSUMER_KEY)
+      @secret = ENV.fetch(OSU_API_CONSUMER_SECRET)
+      @url = ENV.fetch(OSU_API_URL)
+    end
+
+    def directory_query(name)
+      url = "#{ENV.fetch(OSU_API_DIRECTORY_SEARCH)}?q=#{CGI.escape(name)}"
+      logger.debug("OsuApi::Client directory query : #{url}")
+      json = get(url)
+      json['data'].map { |p| Person.new(p['attributes']) }
+    end
+
+    private
+
+    # A default logger - a subclass can override the default
+    # @return [Logger]
+    def logger
+      @logger ||= Rails.logger
+    end
+
+    # :nocov:
+    def connection
+      @connection ||= Faraday.new(url: @url) do |faraday|
+        faraday.use Faraday::Response::RaiseError
+        faraday.adapter Faraday.default_adapter
+      end
+    end
+
+    def url_encoded_connection
+      @url_encoded_connection ||= Faraday.new(url: @url) do |faraday|
+        faraday.use Faraday::Response::RaiseError
+        faraday.request :url_encoded
+        faraday.adapter Faraday.default_adapter
+      end
+    end
+
+    ##
+    # Fetch an OAuth2 token and cache it
+    # since the API sets these keys to be valid for 24hr.
+    def token
+      @token ||= Rails.cache.fetch('osu_api_token', expires_in: 23.hours) do
+        logger.debug("Fetching new OAuth2 Token from OSU API : #{@url}")
+        json = post(url_encoded_connection,
+                    ENV.fetch(OSU_API_OAUTH2_TOKEN),
+                    client_id: @key,
+                    client_secret: @secret,
+                    grant_type: 'client_credentials')
+        json['access_token']
+      end
+    end
+
+    def get(url)
+      response = connection.get url, {}, headers
+      raise response.reason_phrase unless response.success?
+      logger.debug("OSU API GET Response.body: #{response.body}")
+      JSON.parse(response.body)
+    end
+
+    def post(connection, url, data)
+      response = connection.post(url, data)
+      JSON.parse(response.body)
+    end
+
+    def headers(content_type = '')
+      headers = { 'Authorization' => "Bearer #{token}" }
+      headers['Content-Type'] = content_type unless content_type.blank?
+      headers
+    end
+    # :nocov:
+  end
+end
---
+++ lib/osu_api/person.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module OsuApi
+
+  ##
+  # Person object from the OSU Directory API
+  class Person
+    attr_accessor :first_name, :last_name, :primary_affiliation, :email_address
+    def initialize(attributes)
+      @first_name = attributes['firstName']
+      @last_name = attributes['lastName']
+      @primary_affiliation = attributes['primaryAffiliation']
+      @email_address = attributes['emailAddress']
+    end
+  end
+end
