62130b6d8ec74a62c818e642264709f0a0e6be64
Hui Zhang <hui.zhang@oregonstate.edu>
2018-07-25 18:06:57 -0700

    - improve style by Hound suggestions - add spec for fetch author emails job - use 'real' uid in spec factories

--- app/jobs/fetch_authors_emails_wos_job.rb
+++ app/jobs/fetch_authors_emails_wos_job.rb
@@ -1,14 +1,19 @@
+# frozen_string_literal: true
+
+##
+# Fetch authors emails from Web of Science full record and
+# save them to AuthorPublication
 class FetchAuthorsEmailsWosJob < ApplicationJob
   # Defaults to 0
-  #job_options retry: 0
+  # job_options retry: 0
 
   def perform(publication:, current_user: nil, previous_job: nil)
     job = previous_job || Job.create(Job::FETCH_AUTHORS_EMAILS_WOS)
 
     job.update(
-        publication: publication,
-        message: "Attempting to fetch authors emails from Web of Science at #{DateTime.now}",
-        status: Job::STARTED[:name]
+      publication: publication,
+      message: "Attempting to fetch authors emails from Web of Science at #{Time.now}",
+      status: Job::STARTED[:name]
     )
 
     current_user.jobs << job if current_user
@@ -21,16 +26,16 @@ class FetchAuthorsEmailsWosJob < ApplicationJob
     message = "Found #{emails.count} author emails in Web of Science full records." if emails.length
     job.completed(message: message, restartable: false)
   rescue StandardError => e
-    msg = "FetchAuthorsEmailsWosJob.perform"
+    msg = 'FetchAuthorsEmailsWosJob.perform'
     NotificationManager.log_exception(logger, msg, e)
-    job.error({message: "#{msg} : #{e.message}"})
+    job.error(message: "#{msg} : #{e.message}")
   end
 
   private
 
   def fetch_authors_emails(publication)
-    WebOfScience::FetchAuthorsEmailsWos.fetch_from_api(publication)
     logger.info('Fetch authors emails from Web of Science full record complete')
+    WebOfScience::FetchAuthorsEmailsWos.fetch_from_api(publication)
   end
 
   def create_or_update_publication_emails(emails, publication)
--- spec/factories/web_of_science_source_record.rb
+++ spec/factories/web_of_science_source_record.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :web_of_science_source_record do
-    uid 'WOS:1234567890'
+    uid 'WOS:000303060800030'
     active true
     database 'WOS'
     source_data File.read(Rails.root.join('spec/factories/web_of_science_source_record.xml'))
