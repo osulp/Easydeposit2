8ceb29e85b3ef47e57f97ffa2d7530208340f012
Josh Gum <revgum@gmail.com>
2018-10-04 11:25:22 -0700

    fixes spec

--- spec/mailers/publish_mailer_spec.rb
+++ spec/mailers/publish_mailer_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe PublishMailer, type: :mailer do
     let(:params) { { user: user, publication: publication, email: user.email } }
     it 'sends the email' do
       expect(mail.subject).to eq("#{publication.web_of_science_source_record[:uid]} work published to repository.")
-      expect(mail.to).to eq user.email
+      expect(mail.to).to eq [user.email]
     end
   end
 end
