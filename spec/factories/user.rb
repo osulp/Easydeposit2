60a1c62f01f38526597c3e6740549129e8a199a2
Hui Zhang <hui.zhang@oregonstate.edu>
2019-03-21 14:22:44 -0700

    fix security alert: (#225)
    * fix security alert:
    Upgrade actionview to version 5.2.2.1 or later
    Upgrade railties to version 5.2.2.1 or later
    Upgrade bootstrap to version 4.3.1 or later.
    
    * Change factory definition for Factory Bot 5.0
    
    * change factory bot definition

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,16 +1,16 @@
 FactoryBot.define do
   factory :user do
-    email 'bob@ross.com'
-    password 'blahblahblah'
-    username 'Bob Ross'
-    admin false
+    email { "bob@ross.com" }
+    password { "blahblahblah" }
+    username { "Bob Ross" }
+    admin { false }
   end
 
   # This will use the User class (Admin would have been guessed)
   factory :admin, class: User do
-    username 'Bob Ross'
-    password 'blahblahblah'
-    email 'bob@ross.com'
-    admin true
+    username { "Bob Ross" }
+    password { "blahblahblah" }
+    email { "bob@ross.com" }
+    admin { true }
   end
 end
