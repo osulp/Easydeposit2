4cc0fd4f5b67aee1e2fcf40dc3816f4da3f9616b
Ryan Ordway <ryan.ordway@oregonstate.edu>
2021-08-19 15:07:10 -0700

    honeycomb initializer

--- config/initializers/honeycomb.rb
+++ config/initializers/honeycomb.rb
@@ -10,5 +10,24 @@ else
   Honeycomb.configure do |config|
     config.write_key = ENV.fetch('HONEYCOMB_WRITEKEY', 'hereisareallylonglookingkey')
     config.dataset = ENV.fetch('HONEYCOMB_DATASET', 'ed2-test')
+    config.notification_events = %w[
+      sql.active_record
+      render_template.action_view
+      render_collection.action_view
+      process_action.action_controller
+      send_file.action_controller
+      send_data.action_controller
+      deliver.action_mailer
+    ].freeze
+
+  # Scrub unused, private data
+  config.presend_hook do |fields|
+    if fields.key?("redis.command")
+      fields["redis.command"] = fields["redis.command"].slice(0, 300)
+    elsif fields.key?("sql.active_record.binds")
+      fields.delete("sql.active_record.binds")
+      fields.delete("sql.active_record.type_casted_binds")
+    end
+  end
   end
 end
